9c56dd7a9fe0dcc37e61d5edd9e0c952b81285b8
Manna Kong <mkong@redhat.com>
2022-04-13 11:47:15 -0500

    metrics via prometheus client and experter

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ from .seeds import seed_commands
 from .config import Config
 
 from prometheus_flask_exporter import PrometheusMetrics
-
+from prometheus_client import generate_latest
 
 logging.basicConfig(level=logging.INFO)
 logging.info("Setting LOGLEVEL to INFO")
@@ -84,18 +84,20 @@ def inject_csrf_token(response):
     return response
 
 
+@app.route('/flask-prometheus-grafana-example/')
+def hello():
+    return jsonify(say_hello())
+def say_hello():
+    return {'message': 'hello'}
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest()
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
     return app.send_static_file('index.html')
-
-
-@app.route('/flask-prometheus-grafana-example/')
-def hello():
-    return jsonify(say_hello())
-
-
-def say_hello():
-    return {'message': 'hello'}
